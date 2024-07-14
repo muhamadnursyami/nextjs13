a7caa29540eaacb550e1500a2565124e2b18b029
muhamadnursyami <nursami385@gmail.com>
2024-07-14 21:16:36 +0700

    setup project next js

---
+++ src/app/about/profile/Navbar.tsx
@@ -0,0 +1,8 @@
+//  Ini akan jadi components
+export default function Navbar (){
+    return(
+        <>
+        <h1>Ini adalah Navbar Profile</h1>
+        </>
+    )
+}
---
+++ src/app/about/profile/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+// Ini akan Menjadi Route Karna mewakili page dalam Folder Profile
+export default function ProfilePage (){
+    return (
+        <>
+        <Navbar></Navbar>
+        <h1>Halaman Profile</h1>
+        </>
+    )
+}
