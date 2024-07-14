d518b8ba4974adcdee2b44aca0b2e4a146bfd1b5
muhamadnursyami <nursami385@gmail.com>
2024-07-14 22:29:43 +0700

    layout

--- src/app/about/profile/Navbar.tsx
+++
@@ -1,8 +0,0 @@
-//  Ini akan jadi components
-export default function Navbar (){
-    return(
-        <>
-        <h1>Ini adalah Navbar Profile</h1>
-        </>
-    )
-}
--- src/app/about/profile/page.tsx
+++ src/app/about/profile/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from "./Navbar";
+import Navbar from "../../Navbar";
 // Ini akan Menjadi Route Karna mewakili page dalam Folder Profile
 export default function ProfilePage (){
     return (
         <>
-        <Navbar></Navbar>
+        
         <h1>Halaman Profile</h1>
         </>
     )
